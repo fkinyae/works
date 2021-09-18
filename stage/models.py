17923b2a1d7b030899650601afc05df48ddbb112
Francis Kinyae <fmkinyae@gmail.com>
2021-09-18 11:37:11 +0300

    add user profile model

--- stage/models.py
+++ stage/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    bio=models.CharField(max_length=500,null=True,blank=True)
+    profile_pic=models.ImageField(upload_to="profile_pics/",null=True,blank=True)
+    twitter_url=models.CharField(max_length=500,null=True,blank=True)
+
+    
+    def __str__(self):
+        return str(self.user)
+
+
     
     
 
