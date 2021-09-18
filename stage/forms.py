9b5635e8765bb90ba8ff78604a4992fb74e1d0cf
Francis Kinyae <fmkinyae@gmail.com>
2021-09-18 17:01:29 +0300

    add user update and profile update form

--- stage/forms.py
+++ stage/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from django.views import generic
 from django.views.generic import UpdateView
+from .models import Profile
 
 
 class UserSignUpForm(UserCreationForm):
@@ -12,7 +13,25 @@ class UserSignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
         
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields = [
+            'bio',
+            'profile_pic',
+            'twitter_url'
+        ]
+        
+class ProfileUpdateForm(forms.ModelForm):
+            class Meta:
+                model=Profile
+                fields = ['bio','profile_pic','twitter_url']
+                
 
+class UserUpdateForm(forms.ModelForm):
+            class Meta:
+                model=User
+                fields = ['username','email']                
        
 
         
