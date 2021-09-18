3e9ce01aff3750f7a512f1eadbb1f3fb3dd5df87
Francis Kinyae <fmkinyae@gmail.com>
2021-09-18 17:03:04 +0300

    add view for editing user profile

--- stage/views.py
+++ stage/views.py
@@ -1,3 +1,5 @@
+from decimal import Context
+from django.contrib.auth import forms
 from django.shortcuts import render,redirect
 from  django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
@@ -7,7 +9,8 @@ from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.urls import reverse
 from django.urls.base import reverse
-from .forms import UserSignUpForm
+from django.views.generic.base import TemplateView
+from .forms import UserSignUpForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -17,8 +20,17 @@ from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 from django.utils.encoding import force_bytes,force_text,DjangoUnicodeDecodeError
 from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.views.generic.edit import UpdateView
+from .models import Profile
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-
+class edit_profile(generic.UpdateView):
+    model=Profile
+    template='accounts/edit_profile.html'
+    fields=['bio','profile_pic','twitter_url']
+    success_url=reverse_lazy('')
+         
 def welcome(request):
     return render(request, "index.html")
 
@@ -86,18 +98,32 @@ def logout_view(request):
         logout(request)
         
 def view_profile(request):
+ 
+    return render (request, "accounts/profile.html")   
+
+def edit_profile(request):
+    if request.method=='POST':
+        user_form=UserUpdateForm(request.POST, instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form=UserUpdateForm(instance=request.user)   
+        profile_form=ProfileUpdateForm(instance=request.user.profile)
+
     context = {
-        'user':request.user
-    }    
-    return render (request, "accounts/profile.html", context)    
+        "user_form":user_form,
+        "profile_form":profile_form,
         
-        
-        
-
-       
-
+    }
+    return render (request, "accounts/edit_profile.html",context)   
 
     
+   
+       
+   
 class    PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangeForm
     success_url = reverse_lazy('welcome')
