df23e2bcb6c7ce041b143aaee2d089c75b8a5177
Francis Kinyae <fmkinyae@gmail.com>
2021-09-18 11:37:42 +0300

    add path to the profile page

--- stage/urls.py
+++ stage/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('password_change/', views.PasswordsChangeView.as_view(), name='password_change'),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    
+    path('profile/',views.view_profile, name='profile'),   
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
