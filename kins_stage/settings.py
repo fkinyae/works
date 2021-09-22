1772bf62b91acfa77fffa2f9e6cdd63abfa64c79
Francis Kinyae <fmkinyae@gmail.com>
2021-09-22 08:40:52 +0300

    add migrations

--- kins_stage/settings.py
+++ kins_stage/settings.py
@@ -34,8 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'stage',
     'bootstrap4',
-    'tinymce',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
